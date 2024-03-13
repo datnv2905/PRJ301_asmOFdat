b4358bfae05985f139aad5d21a12649afa5bca28
datnv2905 <datbn5602@gmail.com>
2024-03-13 14:32:00 +0700

    create att

---
+++ ASMPRJ301/src/java/controller/lecturer/AttendenceTakingController.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.lecturer;
+
+import dal.LessionDBContext;
+import dal.StudentDBContext;
+import entity.Attendence;
+import entity.Lession;
+import entity.Students;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name="AttendenceTakingController", urlPatterns={"/lecturer/att"})
+public class AttendenceTakingController extends HttpServlet {
+   
+   
+
+  
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+      String leid = req.getParameter("lesid"); 
+        LessionDBContext lesDB = new LessionDBContext();
+        ArrayList<Attendence> atts = lesDB.getAttendencesByLession(leid);
+        req.setAttribute("atts", atts);
+        req.getRequestDispatcher("../view/lecturer/att.jsp").forward(req, resp);
+
+    } 
+
+   
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        String lesid = req.getParameter("lesid");
+        StudentDBContext db = new StudentDBContext();
+        ArrayList<Students> students = db.getStudentsByLessionId(lesid);
+        Lession lession = new Lession();
+        lession.setLesid(lesid);
+        ArrayList<Attendence> atts = new ArrayList<>();
+        for (Students student : students) {
+            Attendence att = new Attendence();
+            att.setStudent(student);
+            att.setLession(lession);
+            att.setDescription(req.getParameter("description" + student.getSid()));
+            att.setPresent(req.getParameter("present" + student.getSid()).equals("yes"));
+           
+            atts.add(att);
+        }
+        LessionDBContext lesDB = new LessionDBContext();
+        lesDB.takeAttendance(lesid, atts);
+        resp.sendRedirect("att?lesid=" + lesid);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- ASMPRJ301/web/view/lecturer/att.jsp
+++ ASMPRJ301/web/view/lecturer/att.jsp
@@ -5,6 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,6 +13,36 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <form action="att" method="POST">
+            <input type="hidden" name="lesid" value="${param.lesid}"/>
+            <table border="1px">
+                <tr>
+                    <td>Id</td>
+                    <td>Name</td>
+                    <td>Presented</td>
+                    <td>Description</td>
+                    <td>Time</td>
+                </tr>
+                <c:forEach items="${requestScope.atts}" var="a">
+                 <tr>
+                    <td>${a.student.sid}</td>
+                    <td>${a.student.sname}</td>
+                    <td>
+                        <input 
+                               ${!a.present?"checked=\"checked\"":""}
+                               type="radio" value="no" name="present${a.student.sid}"/> No
+                        <input type="radio"
+                               ${a.present?"checked=\"checked\"":""}
+                               value="yes" name="present${a.student.sid}" /> Yes
+                    </td>
+                    <td>
+                        <input name="description${a.student.sid}" type="text" value="${a.description}"/>
+                    </td>
+                    <td>${a.time}</td>
+                </tr>   
+                </c:forEach>
+            </table>
+            <input type="submit" value="Save"/>
+        </form>
     </body>
 </html>
