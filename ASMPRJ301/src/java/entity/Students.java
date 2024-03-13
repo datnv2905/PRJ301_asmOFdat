39e2affd6e40b18472755c7814c27d8b4c39b9dc
datnv2905 <datbn5602@gmail.com>
2024-03-13 15:03:55 +0700

    update image of students

--- ASMPRJ301/src/java/entity/Students.java
+++ ASMPRJ301/src/java/entity/Students.java
@@ -14,15 +14,18 @@ public class Students {
     private String address;
     private String phone;
     private String email;
+    private String img;
 
-    public Students(String sid, String sname, String address, String phone, String email) {
+    public Students(String sid, String sname, String address, String phone, String email, String img) {
         this.sid = sid;
         this.sname = sname;
         this.address = address;
         this.phone = phone;
         this.email = email;
+        this.img = img;
     }
 
+   
     public Students() {
     }
 
@@ -64,6 +67,14 @@ public class Students {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
     }
     
     
